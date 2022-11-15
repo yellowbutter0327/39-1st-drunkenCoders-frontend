89c1a814958606e6bf5669eeebafe957eae97e39
jjongbum92 <jjongbum92@gmail.com>
2022-11-15 15:32:10 +0900

    main 브랜치 변경

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,110 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import iconFacebook from './../../assets/footer/icon_facebook.png';
+import iconInstagram from './../../assets/footer/icon_instagram.png';
+import iconKakao from './../../assets/footer/icon_kakao.png';
+import iconNaver from './../../assets/footer/icon_naver.png';
+import iconYoutube from './../../assets/footer/icon_youtube.png';
+
+import './Footer.scss';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="inner">
+        <div className="company-info">
+          <div className="box">
+            <p className="tit">담화컴퍼니 주식회사</p>
+            <ul className="list">
+              <li>고객센터 : 070-5014-1282</li>
+              <li>평일 10:00 ~ 18:00, 주말 휴무</li>
+            </ul>
+          </div>
+          <div className="sns-box">
+            <a
+              href="https://www.instagram.com/sooldamhwa/?hl=ko"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={iconInstagram} alt="인스타그램" />
+            </a>
+            <a
+              href="https://www.youtube.com/channel/UCuhTvdO3VD-S3md_CkWYtbw"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={iconYoutube} alt="유튜브" />
+            </a>
+            <a
+              href="http://pf.kakao.com/_xktAgj"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={iconKakao} alt="카카오" />
+            </a>
+            <a
+              href="https://www.facebook.com/sooldamhwa/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={iconFacebook} alt="페이스북" />
+            </a>
+            <a
+              href="https://blog.naver.com/sooldamhwa"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={iconNaver} alt="네이버" />
+            </a>
+          </div>
+        </div>
+        <ul className="link-area">
+          <li>
+            <Link to="/">이용약관</Link>
+          </li>
+          <li>
+            <Link to="/">개인정보처리방침</Link>
+          </li>
+          <li>
+            <Link to="/">입점문의</Link>
+          </li>
+          <li>
+            <Link to="/">기업 구매</Link>
+          </li>
+        </ul>
+        <div className="info-area">
+          <ul>
+            <li>대표 : 이재욱</li>
+            <li>
+              사업자등록번호 : 620-81-58299 <a href="">사업자확인</a>
+            </li>
+            <li>통신판매 신고번호 : 2021-서울서초-2084</li>
+          </ul>
+          <ul>
+            <li>
+              주소 : 서울특별시 서초구 강남대로 527, 10층(브랜드칸타워, 술담화)
+            </li>
+            <li>정보보호 책임자 : 박준형</li>
+            <li>대표 전화 : 070-5014-1282</li>
+            <li>이메일 : info@sooldamhwa.com</li>
+          </ul>
+        </div>
+        <p class="sub-txt">
+          술담화는 통신판매중개자로서 통신판매 당사자가 아니며, 판매자가 등록한
+          상품정보 및 거래에 대해 술담화는 책임을 지지 않습니다.
+          <br />
+          고객님의 안전거래를 위해 현금 등으로 결제 시 저희 쇼핑몰에서 가입한
+          NICE구매안전 (에스크로) 서비스를 이용하실 수 있습니다.
+        </p>
+        <a
+          className="service-link"
+          href="https://npg.nicepay.co.kr/issue/issueEscrow.do?Mid=IMPsoold1m&amp;CoNo=6208158299"
+        >
+          서비스가입사실 확인
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;

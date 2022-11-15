bdd4de1678f1ec24de23b08efbd320e38534b9f5
jjongbum92 <jjongbum92@gmail.com>
2022-11-15 17:10:12 +0900

    MODIFY:nav, footer 제거

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,39 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from './../../assets/nav/mainLogo.png';
-import searchIcon from './../../assets/nav/searchIcon.svg';
-
-import './Nav.scss';
 
 const Nav = () => {
-  return (
-    <div className="header">
-      <div className="inner">
-        <h1>
-          <Link to="/">
-            <img src={logo} alt="술담화 로고" />
-          </Link>
-        </h1>
-        <div className="link-area">
-          <Link to="/">구독서비스</Link>
-          <Link to="/" className="active">
-            담화마켓(스토어)
-          </Link>
-        </div>
-        <Link to="/search" className="search-area">
-          <img src={searchIcon} alt="검색 아이콘" />
-          <span className="txt">무엇을 찾고 계신가요?</span>
-        </Link>
-        <div className="info-area">
-          <Link to="/login">로그인</Link>
-          <Link to="/cart" className="cart">
-            장바구니
-            <span className="num">1</span>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>Nav</div>;
 };
 
 export default Nav;

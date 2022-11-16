d75c3a091bc6b6cac65edb09fedf3b4f9dce6e96
“yellowbutter03” <yellowbutter0327@gmail.com>
2022-11-16 22:43:41 +0900

    Add: 로그인 페이지 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -10,69 +10,62 @@ const Login = () => {
 
   return (
     <div className="container">
-      <div className="login-container">
-        <h4 className="title-login">로그인</h4>
+      {/* <div className="login-container"> */}
+      <h4 className="title-login">로그인</h4>
 
-        <div className="login-form">
-          <div className="login-inputemail">
-            <label className="title-email" htmlFor="input-email">
-              이메일
-            </label>
+      <div className="login-form">
+        <form>
+          <div className="form-wrap">
+            <label htmlFor="input-email">이메일</label>
             <input
               // onChange={getUserInfo}
               id="input-email"
-              className="id-input"
               name="userId"
               type="text"
               placeholder="이메일을 입력해주세요"
             />
           </div>
 
-          <div>
-            <label className="title-password" htmlFor="input-password">
-              비밀번호
-            </label>
+          <div className="form-wrap">
+            <label htmlFor="input-password">비밀번호</label>
             <input
               // onChange={getUserInfo}
               id="input-password"
-              className="password-input"
               name="userPassword"
               type="text"
               placeholder="비밀번호를 입력해주세요"
             />
           </div>
 
-          <form>
-            <button className="login-button" type="submit">
-              로그인
-            </button>
-          </form>
+          <button className="login-button" type="submit">
+            로그인
+          </button>
+        </form>
 
-          <div className="signup">
-            <Link to="/register">회원가입(최대 4000p적립)</Link>
-          </div>
+        <div className="signup">
+          <Link to="/register">회원가입(최대 4000p적립)</Link>
+        </div>
 
-          <div className="social-button">
-            <button className="loginButton-kakao" type="submit">
-              <img src={iconKakao} />
-              카카오 로그인
-            </button>
-            <button className="loginButton-naver" type="submit">
-              <img src={iconNaver} />
-              네이버 로그인
-            </button>
-            <button className="loginButton-google" type="submit">
-              <img src={iconGoogle} />
-              구글 로그인
-            </button>
-          </div>
+        <div className="social-button">
+          <button className="loginButton-kakao" type="submit">
+            <img src={iconKakao} />
+            카카오 로그인
+          </button>
+          <button className="loginButton-naver" type="submit">
+            <img src={iconNaver} />
+            네이버 로그인
+          </button>
+          <button className="loginButton-google" type="submit">
+            <img src={iconGoogle} />
+            구글 로그인
+          </button>
+        </div>
 
-          <div className="find-id">
-            <Link to="/login">아이디 찾기</Link>
-          </div>
-          <div className="find-password">
-            <Link to="/register">비밀번호 찾기</Link>
-          </div>
+        <div className="link-find">
+          <Link to="/login">아이디 찾기</Link>
+        </div>
+        <div className="link-find">
+          <Link to="/register">비밀번호 찾기</Link>
         </div>
       </div>
     </div>

21755c57dd11c9005208b5a4eab4e212b8cde0c5
jjongbum92 <jjongbum92@gmail.com>
2022-11-17 13:50:36 +0900

    MODIFY: 수정사항 반영

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Footer.scss';
 import Sns from './component/Sns';
+import './Footer.scss';
 
 const Footer = () => {
   return (
--- src/components/Footer/component/Sns.js
+++ src/components/Footer/component/Sns.js
@@ -1,50 +1,18 @@
 import React from 'react';
-import './Sns.scss';
 import iconInstagram from './../../../assets/footer/icon_instagram.png';
 import iconYoutube from './../../../assets/footer/icon_youtube.png';
 import iconKakao from './../../../assets/footer/icon_kakao.png';
 import iconFacebook from './../../../assets/footer/icon_facebook.png';
 import iconNaver from './../../../assets/footer/icon_naver.png';
+import './Sns.scss';
 
 const Sns = () => {
-  const snsLink = [
-    {
-      id: 1,
-      name: '인스타그램',
-      icon: iconInstagram,
-      url: 'https://www.instagram.com/sooldamhwa/?hl=ko',
-    },
-    {
-      id: 2,
-      name: '유튜브',
-      icon: iconYoutube,
-      url: 'https://www.youtube.com/channel/UCuhTvdO3VD-S3md_CkWYtbw',
-    },
-    {
-      id: 3,
-      name: '카카오',
-      icon: iconKakao,
-      url: 'http://pf.kakao.com/_xktAgj',
-    },
-    {
-      id: 4,
-      name: '페이스북',
-      icon: iconFacebook,
-      url: 'https://www.facebook.com/sooldamhwa/',
-    },
-    {
-      id: 5,
-      name: '네이버',
-      icon: iconNaver,
-      url: 'https://blog.naver.com/sooldamhwa',
-    },
-  ];
   return (
     <div className="sns-box">
-      {snsLink.map(ele => {
+      {snsList.map(sns => {
         return (
-          <a href={ele.url} target="_blank" rel="noreferrer" key={ele.id}>
-            <img src={ele.icon} alt={ele.name} />
+          <a href={sns.url} target="_blank" rel="noreferrer" key={sns.id}>
+            <img src={sns.icon} alt={sns.name} />
           </a>
         );
       })}
@@ -53,3 +21,36 @@ const Sns = () => {
 };
 
 export default Sns;
+
+const snsList = [
+  {
+    id: 1,
+    name: '인스타그램',
+    icon: iconInstagram,
+    url: 'https://www.instagram.com/sooldamhwa/?hl=ko',
+  },
+  {
+    id: 2,
+    name: '유튜브',
+    icon: iconYoutube,
+    url: 'https://www.youtube.com/channel/UCuhTvdO3VD-S3md_CkWYtbw',
+  },
+  {
+    id: 3,
+    name: '카카오',
+    icon: iconKakao,
+    url: 'http://pf.kakao.com/_xktAgj',
+  },
+  {
+    id: 4,
+    name: '페이스북',
+    icon: iconFacebook,
+    url: 'https://www.facebook.com/sooldamhwa/',
+  },
+  {
+    id: 5,
+    name: '네이버',
+    icon: iconNaver,
+    url: 'https://blog.naver.com/sooldamhwa',
+  },
+];
